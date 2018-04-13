dbbb359af28d4904e87efd5675be65f55e689e6e
ericdecanini <pandakunvroductions@gmail.com>
2018-04-13 09:29:22 +0100

    Initial commit

---
+++ app/src/main/java/myvaluelist/eddecanini/myvaluelist/MainActivity.java
@@ -0,0 +1,13 @@
+package myvaluelist.eddecanini.myvaluelist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
